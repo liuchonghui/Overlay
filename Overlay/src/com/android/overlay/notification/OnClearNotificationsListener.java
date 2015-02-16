11fa16099c7269f88b663b3aa3dea62fea57e943
liuchonghui <liuchonghui@139.com>
2015-02-16 15:10:18 +0800

    Add OnClearNotificationsListener.java for a ManagerInterface.

---
+++ Overlay/src/com/android/overlay/notification/OnClearNotificationsListener.java
@@ -0,0 +1,9 @@
+package com.android.overlay.notification;
+
+import com.android.overlay.BaseManagerInterface;
+
+public interface OnClearNotificationsListener extends BaseManagerInterface {
+
+	void clearNotifications();
+
+}
